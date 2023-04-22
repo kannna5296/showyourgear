11e09de90773bcbe052db00fbd0e67b780e7420d
kannna5296 <kannnahacks5296@gmail.com>
2023-04-23 00:03:11 +0900

    ギア検証APIの枠だけ作成

---
+++ apis/src/main/java/verifygear/Application.kt
@@ -0,0 +1,33 @@
+package verifygear
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import java.io.IOException
+
+/**
+ * Handler for requests to Lambda function.
+ */
+class Application : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    override fun handleRequest(input: APIGatewayProxyRequestEvent?, context: Context?): APIGatewayProxyResponseEvent {
+        val headers: MutableMap<String, String> = mutableMapOf()
+        headers["Content-Type"] = "application/json"
+        headers["X-Custom-Header"] = "application/json"
+
+        val response: APIGatewayProxyResponseEvent = APIGatewayProxyResponseEvent()
+            .withHeaders(headers)
+
+        return try {
+            val output: String = String.format("{ \"message\": \"Calling postPhoto!\"}")
+            response
+                .withStatusCode(200)
+                .withBody(output)
+        } catch (e: IOException) {
+            response
+                .withBody("{}")
+                .withStatusCode(500)
+        }
+    }
+}
