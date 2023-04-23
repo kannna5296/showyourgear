a9fe927184233e9b476f89e2834bc4ca9d6784f8
kannna5296 <kannnahacks5296@gmail.com>
2023-04-23 19:00:51 +0900

    fmt

--- apis/src/test/java/getphotos/ApplicationTest.kt
+++ apis/src/test/java/getphotos/ApplicationTest.kt
@@ -1,15 +1,21 @@
 package getphotos
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
 class ApplicationTest {
-//    @Test
-//    fun successfulResponse() {
-//        val app = Application()
-//        val result: APIGatewayProxyResponseEvent = app.handleRequest(null, null)
-//        assertEquals(200, result.statusCode)
-//        assertEquals("application/json", result.headers["Content-Type"])
-//        val content: String = result.body
-//        assertNotNull(content)
-//        assertTrue(content.contains("\"message\""))
-//        assertTrue(content.contains("\"Calling getPhotos!\""))
-//    }
+    @Test
+    fun successfulResponse() {
+        val app = Application()
+        val result: APIGatewayProxyResponseEvent = app.handleRequest(null, null)
+        assertEquals(200, result.statusCode)
+        assertEquals("application/json", result.headers["Content-Type"])
+        val content: String = result.body
+        assertNotNull(content)
+        assertTrue(content.contains("\"message\""))
+        assertTrue(content.contains("\"Calling getPhotos!\""))
+    }
 }
