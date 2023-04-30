edd8f356dfc1a683dc0e329b468d5d6db1d33810
kannna5296 <kannnahacks5296@gmail.com>
2023-04-30 18:16:39 +0900

    src返す

--- apis/src/test/kotlin/getphotos/ApplicationTest.kt
+++ apis/src/test/kotlin/getphotos/ApplicationTest.kt
@@ -15,7 +15,5 @@ class ApplicationTest {
         assertEquals("application/json", result.headers["Content-Type"])
         val content: String = result.body
         assertNotNull(content)
-        assertTrue(content.contains("\"message\""))
-        assertTrue(content.contains("\"Calling getPhotos!\""))
     }
 }
